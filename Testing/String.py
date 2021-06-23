7c23b2fadf62d5b02a70448417583661f572f714
Jonathan Chaves <542073+jonathanchc@users.noreply.github.com>
2021-06-23 09:44:26 -0600

    Adding String code

---
+++ Testing/String.py
@@ -0,0 +1,30 @@
+#string python
+#string manipulation options
+#We can define a string as a chain of characters, and to manipulate and show those  there is  a variety of options
+
+print('single quoted')
+
+print("double quoted")
+
+
+print('''Multi
+    Line
+    string
+     ''')
+
+
+##Concatenation is the way to connect multiple strings
+print("Hello "+ "Wold")
+
+str_concatenated = "Python " + " computer"
+
+print(str_concatenated)
+
+##Strings manipulation
+str_gretting="Good Monrning "
+print(str_gretting * 3 )
+
+
+#find values.
+
+str_gretting.find('o')
--- Testing/script.py
+++ Testing/script.py
@@ -1,5 +1,12 @@
+#Inintial This a comment
 1+1
+r=""" Multiple Line  Comment
+
+"""
 print("Hello World File")
+print(r)
 1+1
 a=1+1
 print(a)
+
+#This a comment 

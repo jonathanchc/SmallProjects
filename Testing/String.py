bf767938de7216345c8d3a7cc9da1464c49c3646
Jonathan Chaves <542073+jonathanchc@users.noreply.github.com>
2021-06-23 09:58:28 -0600

    Examples

--- Testing/String.py
+++ Testing/String.py
@@ -1,5 +1,6 @@
 #string python
-#string manipulation options
+#https://docs.python.org/3/library/stdtypes.html#text-sequence-type-str
+#Some string manipulation options
 #We can define a string as a chain of characters, and to manipulate and show those  there is  a variety of options
 
 print('single quoted')
@@ -25,6 +26,28 @@ str_gretting="Good Monrning "
 print(str_gretting * 3 )
 
 
-#find values.
+#Making the text in upper case.
+print(str_gretting.upper)
 
-str_gretting.find('o')
+#Find values in  string
+
+print("\n")
+
+
+#the result is a integer with the position
+str_find=str_gretting.find('M')
+
+#transforming the integer into string using the str() method
+print("The Letter M is in the position " + str(str_find))
+print("\n")
+#Adding spaces or tabs in the string.
+print("Tab\t" + str_gretting)
+print("\n")
+#Adding new Line in the string.
+print("New Line\n" + str_gretting)
+
+#print  quotes
+print("'quotes'")
+print('"Double quotes"')
+
+

2edb17e6ccc52fcee346dad07bed87964ca67098
Jonathan Chaves <542073+jonathanchc@users.noreply.github.com>
2021-06-23 11:14:35 -0600

    Math

---
+++ Testing/numbers.py
@@ -0,0 +1,19 @@
+#https://docs.python.org/3/library/stdtypes.html#numeric-types-int-float-complex
+#Do Math in Python is very similar to other programming languages.
+#Few examples
+
+a=2+2
+
+print("result a=2+2 =" + str(a)  )
+
+a=10-4
+print("Result of a=10-4 =" + str(a)  )
+
+a=4/2
+print("Result of 4/2 =" + str(a)  )
+
+a=pow(5,2)
+print("Result of a=pow(5,2) =" + str(a)  )
+
+a="5"
+print("Result of string a=5 +1  to int =" + str(int(a) +1)  )
